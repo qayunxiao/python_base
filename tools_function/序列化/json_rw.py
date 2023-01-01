645dc120be7df19291445ddee38e1744eb41a747
alvinsss <6449694@qq.com>
2023-01-01 20:46:29 +0800

    json序列化

--- tools_function/序列化/json_rw.py
+++ tools_function/序列化/json_rw.py
@@ -12,5 +12,5 @@ import  json
 #     print(res.get('address').get('city'))
 
 my_list=[("admin","password成功"),("alvin","123456")]
-with open("jsonw.json",'w',encoding="utf-8") as fw:
+with open( "jsonw.json", 'w', encoding="utf-8" ) as fw:
     json.dump(my_list,fw,ensure_ascii=False,indent=2)#ensure_ascii不已ASCII方式显示,indent缩进
---
+++ tools_function/序列化/package_json.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/1/1 17:00
+# @Author  : alvin
+# @File    : package_json.py
+# @Software: PyCharm
+import json
+
+def read(path):
+    with open(path,'r') as f:
+        data= f.read()
+    return json.loads(data)
+
+def write(path,data):
+    with open(path,"w") as f:
+        if isinstance(data,dict):
+            _data = json.dumps(data)
+            f.write(_data)
+        else:
+            raise  TypeError("data is dict")
+
+data = {"name":"avlv","age":18,"top":179}
+
+def my_wirte_json():
+    with open("my_json.json","w",encoding="utf8") as f:
+        f.write(json.dumps({"name":"海林","age":18,"top":179}))
+
+if __name__ == '__main__':
+    # write("package_json.text",data)
+    # res = read("package_json.text")
+    # print(res,type(res))
+    # res['sex']='b'
+    # print(res)
+    my_wirte_json()

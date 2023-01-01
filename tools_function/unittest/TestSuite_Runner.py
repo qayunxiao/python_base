e25e87095f5891a1d50bd7165f6bb02de381b38e
alvinsss <6449694@qq.com>
2023-01-01 20:47:10 +0800

    文件读写

--- tools_function/unittest/TestSuite_Runner.py
+++ tools_function/unittest/TestSuite_Runner.py
@@ -4,8 +4,7 @@
 # @File    : TestSuite_Runner.py
 # @Software: PyCharm
 from tools_function.unittest.case.TestCase01 import testDemo1
-from tools_function.unittest.case.TestCase02 import testDemo2
-testDemo2
+from tools_function.unittest.case.TestCase02  import testDemo2
 #1、导包
 import unittest
 #2、实例化套件对象
@@ -13,7 +12,7 @@ suite = unittest.TestSuite()
 #3、使用套件对象添加用例方法
 #方式1 套件对象.addTest(测试类名('方法名'))，复制类名，使用自动导包
 suite.addTest(testDemo1('test_method01'))
-suite.addTest(testDemo2('test_method01'))
+suite.addTest(testDemo2('test_method02_1'))
 #4、实例化运行对象
 runner = unittest.TextTestRunner()
 #5、运行套件对象
---
+++ tools_function/unittest/case/TestCase02.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/12/27 15:29
+# @Author  : alvin
+# @File    : TestCase01.py
+# @Software: PyCharm
+#导包
+import  unittest
+#自定义测试类 需继承unittest的模块TestCase类
+class testDemo2(unittest.TestCase):
+    #书写测试方法 必须test开头，建议test_
+    def test_method02_1(self):
+        print("ceshi 2-1")
+    def test_method02_2(self):
+        print("ceshi 2-2")
+# 测试执行
+#光标放在类名后面
+#光标放方法名后面

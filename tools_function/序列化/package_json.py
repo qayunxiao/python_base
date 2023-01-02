22a6b870496785be0ebabfba54d97614e7ea0080
alvinsss <6449694@qq.com>
2023-01-02 11:00:46 +0800

    日志模块 logging.basicConfig

--- tools_function/序列化/package_json.py
+++ tools_function/序列化/package_json.py
@@ -4,7 +4,9 @@
 # @File    : package_json.py
 # @Software: PyCharm
 import json
+from log.package_log import init_log
 
+log = init_log("json.log")
 def read(path):
     with open(path,'r') as f:
         data= f.read()
@@ -16,6 +18,7 @@ def write(path,data):
             _data = json.dumps(data)
             f.write(_data)
         else:
+            log.error("data is dict")
             raise  TypeError("data is dict")
 
 data = {"name":"avlv","age":18,"top":179}
@@ -25,9 +28,9 @@ def my_wirte_json():
         f.write(json.dumps({"name":"海林","age":18,"top":179}))
 
 if __name__ == '__main__':
-    # write("package_json.text",data)
+    write("package_json.text",data)
     # res = read("package_json.text")
     # print(res,type(res))
     # res['sex']='b'
     # print(res)
-    my_wirte_json()
+    # write()

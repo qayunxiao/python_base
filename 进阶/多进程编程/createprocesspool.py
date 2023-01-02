1ec40113c7ed20928ce65e84bc43e638b443a257
alvinsss <6449694@qq.com>
2023-01-02 16:18:16 +0800

    进程池apply_async可以获取返回值

--- 进阶/多进程编程/createprocesspool.py
+++ 进阶/多进程编程/createprocesspool.py
@@ -10,9 +10,15 @@ import time
 def work(count):
     print(count,os.getpid())
     time.sleep(5)
+    return 'res is {},pid is {}'.format(count,os.getpid())
 
 if __name__ == '__main__':
     pool = multiprocessing.Pool(5)#创建为5的进程池
+    results =[]
     for i in range(20):
-        pool.apply_async(func=work,args=(i,))
-    time.sleep(20)
+        result=pool.apply_async(func=work,args=(i,))
+        results.append(result)
+    # pool.close()
+    # pool.join()#等子进程结束之后再主进程结束，保证进程池任务全部结束再退出主进程
+    for _results in results:
+        print(_results.get())

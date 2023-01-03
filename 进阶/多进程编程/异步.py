4b7885fb2cc952f619a8fb2351e02a9cf471d501
alvinsss <6449694@qq.com>
2023-01-03 09:32:17 +0800

    异步

---
+++ 进阶/多进程编程/异步.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/1/3 9:21
+# @Author  : alvin
+# @File    : 异步.py
+# @Software: PyCharm
+import random
+import time
+import asyncio
+
+async def a():
+    for i in range(10):
+        print(i,"a")
+        #time.sleep cpu级别的阻塞
+        await  asyncio.sleep(random.random() *2)
+    return  'a func'
+
+
+async def b():
+    for i in range(10):
+        print(i,"b")
+        await  asyncio.sleep(random.random() *2)
+    return  'b func'
+
+async def main():
+    res =await  asyncio.gather(
+        a(),
+        b()
+    )
+    print("res is:",res)
+
+if __name__ == '__main__':
+    start=time.time()
+    asyncio.run(main())
+    # a()
+    # b()
+    print("time is:{}".format(time.time() - start))

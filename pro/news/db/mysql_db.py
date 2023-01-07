766730cb5789615415141fffef95bdad9c854cf8
alvinsss <6449694@qq.com>
2023-01-07 09:14:12 +0800

    mysql.connector.pooling封装链接池

---
+++ pro/news/db/mysql_db.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/1/7 9:10
+# @Author  : alvin
+# @File    : mysql_db.py
+# @Software: PyCharm
+import  mysql.connector.pooling
+# 私有变量
+__config ={
+    "host":"loclhost",
+    "port":3306,
+    "user":"qa",
+    "password":"qatest",
+    "database":"vega"
+}
+try:
+    pool=mysql.connector.pooling.MySQLConnectionPool(
+        **__config,
+        pool_size=10
+    )
+except Exception as e:
+    print(e)

09fedf4329a2f144e635b791a85ec1dce180bba0
alvinsss <6449694@qq.com>
2023-01-08 14:28:58 +0800

    批量修改替换文件名称

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # 2、app专项
 # 2、app自动化
 pip install colorama 控制台彩色
+ipython
---
+++ 进阶/office/file/file_rename.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/1/8 13:59
+# @Author  : alvin
+# @File    : file_rename.py
+# @Software: PyCharm
+import  glob
+import shutil
+
+
+def update_name(path,replace_str):
+    res = glob.glob(path)
+    # print(res,type(res))
+    for data in res:
+        #如果res的列表中仍然是路径 进行递归
+        if glob.os.path.isdir(data):
+            _path = glob.os.path.join(data,'*')
+            update_name(_path,replace_str)
+        else:
+            #[/home/xxx,name.text]
+            path_list = glob.os.path.split(data)
+            name = path_list[-1]
+            new_name= name.replace(replace_str,"")
+            new_data=glob.os.path.join(path_list[0],new_name)
+            shutil.move(data,new_data)
+
+
+
+if __name__ == '__main__':
+    path = glob.os.path.join(glob.os.getcwd())
+    replace_str="【海量一手：itwangzi.cn】"
+    print(path)
+    update_name("E:\IT\py2022",replace_str)

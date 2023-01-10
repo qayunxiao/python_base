77cd74e64eef34667884237dafc5ff4061d2a245
alvinsss <6449694@qq.com>
2023-01-10 16:30:03 +0800

    进行重复文件名删除操作

--- 进阶/office/myfile/file_remove_updatename.py
+++ 进阶/office/myfile/file_remove_updatename.py
@@ -8,14 +8,14 @@ import os
 import shutil
 import re
 
-def remove_updatename(path,remove_name_list,update_replace):
+def remove_updatename(path,remove_name_list,replace_list):
     #返回路径列表
     res = glob.glob(path)
     for data in res:
         if glob.os.path.isdir(data):
             _path = glob.os.path.join(data,"*")
             #递归
-            remove_updatename(_path,remove_name_list,update_replace)
+            remove_updatename(_path,remove_name_list,replace_list)
         else:#如果不是路径，获取文件名
             path_list = glob.os.path.split(data)
             name = path_list[-1]
@@ -24,12 +24,13 @@ def remove_updatename(path,remove_name_list,update_replace):
                     # print("remove data name",data)
                     os.remove(data)
             #如果需要替换的字符串在文件名里面 去替换成空
-            if update_replace in name:
-                new_name = name.replace(update_replace,"")
-                new_path = glob.os.path.join(path_list[0],new_name)
-                # print("update_replace-data",data )
-                # print("update_replace-new_path",new_path)
-                shutil.move(data,new_path)
+            for update_replace in replace_list:
+                if update_replace in name:
+                    new_name = name.replace(update_replace,"")
+                    new_path = glob.os.path.join(path_list[0],new_name)
+                    # print("update_replace-data",data )
+                    # print("update_replace-new_path",new_path)
+                    shutil.move(data,new_path)
     return "处理完成"
 
 def fix_name(path,sub_start):
@@ -52,10 +53,12 @@ def fix_name(path,sub_start):
     return "处理完成"
 
 if __name__ == '__main__':
-    path =r"D:\IT\3.自动化测试平台开发课程"
-    # remove_name_list=["思索IT获取更多课程.url","资料.url"]
-    # update_replace="【海量资源：sisuoit.com】"
-    # res=remove_updatename(path,remove_name_list,update_replace)
-    # print(res)
-    sub_start=16
-    fix_name(path,sub_start)
+    path =r"D:\IT\柠檬班-Python自动化测试35期"
+    remove_name_list=["思索IT获取更多课程.url","资料.url",
+                      "面试合集.txt","看看我【www.sisuoit.com】.zip",
+                      "软件下载.txt","下载必看【www.sisuoit.com】.txt","资料2.zip"]
+    replace_list=["【海量资源：sisuoit.com】"]
+    res=remove_updatename(path,remove_name_list,replace_list)
+    print(res)
+    # sub_start=16
+    # fix_name(path,sub_start)
--- 进阶/office/myfile/file_test.py
+++ 进阶/office/myfile/file_test.py
@@ -15,7 +15,7 @@ src_file  = os.path.join(os.path.dirname(os.getcwd()),"filecopytree.text")
 tar_file = os.path.join(os.path.dirname(os.getcwd()),"copy")
 # copy(src_file,tar_file)
 tar_copyfile = os.path.join(os.path.dirname(os.getcwd()),"copy","copyfile.text")
-tar_movefile = os.path.join(os.path.dirname(os.getcwd()),"file","move.text")
+tar_movefile = os.path.join(os.path.dirname(os.getcwd()),"myfile","move.text")
 # copyfile(src_file,tar_copyfile)
 #相同路径下属于重命名
 # move("copyfile.text",tar_movefile)
@@ -26,8 +26,8 @@ tar_movefile = os.path.join(os.path.dirname(os.getcwd()),"file","move.text")
 #删除
 # os.remove("remove.text")
 #压缩文件
-print(os.path.join(os.path.dirname(os.getcwd()),"file"))
-# make_archive('tar_file','zip',os.path.join(os.path.dirname(os.getcwd()),"file"))
+print(os.path.join(os.path.dirname(os.getcwd()),"myfile"))
+# make_archive('tar_file','zip',os.path.join(os.path.dirname(os.getcwd()),"myfile"))
 #解压缩
 tar_unpack_archive = os.path.join(os.path.dirname(os.getcwd()),"tar_file")
 # unpack_archive("tar_file.zip",tar_unpack_archive)
@@ -35,6 +35,6 @@ tar_unpack_archive = os.path.join(os.path.dirname(os.getcwd()),"tar_file")
 # copytree("test1",os.path.join(os.path.dirname(os.getcwd()),"copytree"))
 #copy1必须存在
 # rmtree(os.path.join(os.path.dirname(os.getcwd()),"copy1"))
-targets=os.path.join(os.path.dirname(os.getcwd()),"file")
+targets=os.path.join(os.path.dirname(os.getcwd()),"myfile")
 res = glob(targets+'/*.py')
 print("res",res,targets)
